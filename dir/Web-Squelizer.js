fc35afca4193b025c5e792540798d27a8cbd7bdd
lukas102000 <lucasveraz102000@gmail.com>
2023-05-29 21:08:16 +0100

    update

--- dir/Web-Squelizer.js
+++ dir/Web-Squelizer.js
@@ -1,6 +1,17 @@
-class WebSqlite {
-    constructor (_databaseName){
+class WebSquelizer {
+    constructor (_databaseName, callback){
         this.storage = window.openDatabase(_databaseName, "1.0", "com.lucas_veraz", 32*7588*7588)
+        if(_databaseName === "" || _databaseName === " " || _databaseName === "  "){
+            callback("Name is not calable for types")
+        } else{
+            if(typeof(callback) === "function"){
+                if(this.storage){
+                    callback("Database conected...")
+                }
+                
+            }
+        }
+        
     }
     /*
      *   Create the new table names and comluns
@@ -40,7 +51,6 @@ class WebSqlite {
         *   DROP THIS TABLES
         */
         const findAll = (args, callback)=>{
-            
             if( args === ""){
                 
                 this.storage.transaction((ex)=>{
@@ -58,41 +68,61 @@ class WebSqlite {
                 }
             }else{
                 if(typeof(args) === "object"){
-                    console.log(args)
                     let query = ""
                     let exe = ""
                     let order_by = ""
+                    let ands= ""
+                    let like2 =""
                     for(let i= 0; i < Object.keys(args).length; i++){
-                        
                         if(Object.keys(args)[i] === "ORDER BY"){
                             order_by = `${Object.keys(args)[i]} ${Object.values(args)[i]}`
                         } 
-                        else if(Object.keys(args)[i] === "WHERE"){
+                        else if(Object.keys(args)[i] === "WHERE" || Object.keys(args)[i] === "AND" || Object.keys(args)[i] === "OR" ){
                             let key_man = Object.keys(args)[i]
                             let key_sub = Object.values(args)[i]
                             
                             for(let x=0; x < Object.keys(key_sub).length; x++){
-                                exe = `${key_man}`
-                                
-                                if(typeof(Object.values(key_sub)[x]) === "object"){
+                                let key_man = Object.keys(args)[i]
+                                let key_sub2 = Object.keys(key_sub)[0]
+                                if( typeof(Object.values(key_sub)[0]) === "object"){
                                     let keys_sub2 = Object.values(key_sub)[x]
+
+                                    let like = Object.values(key_sub)[x]
                                     
-                                    for(let y =0; y < Object.values(keys_sub2).length; y++){
-                                        query += `${Object.keys(key_sub)[x]} ${Object.keys(keys_sub2)[y]} '${Object.values(keys_sub2)[y]}'''`
+                                    for(let z = 0; z < Object.values(key_sub).length; z++){
+                                        //console.log(Object.keys(key_sub)[z])
+                                        if(typeof(Object.values(key_sub)[z]) === "object"){
+                                            let chose = Object.values(key_sub)[z]
+                                            if(Object.keys(key_sub)[z] === "ORDER BY"){
+                                                like2 = `${Object.keys(key_sub)[z]} ${Object.keys(chose)[0]} ${Object.values(chose)[0]}`
+                                            }
+                                            
+                                        }
+                                       
+
+                                        //like2 += `${key_man} ${Object.keys(key_sub)[z]} ${Object.values(key_sub)[z]} `
+
                                     }
-                                    
+
+                                    if(Object.keys(like)[x] === "LIKE" || Object.keys(like)[x] === "ORDER BY"){
+                                        for(let y =0; y < Object.values(keys_sub2).length; y++){
+                                                query += `${key_man} ${Object.keys(key_sub)[x]} ${Object.keys(keys_sub2)[y]} '${Object.values(keys_sub2)[y]}' ${like2} ''`
+                                                //console.log(`${key_man} ${Object.keys(key_sub)[x]} ${Object.keys(keys_sub2)[y]} '${Object.values(keys_sub2)[y]}'`)
+                                        }
+
+                                    }
                                 }
                                 else{
-                                    query += `${Object.keys(key_sub)[x]} = '${Object.values(key_sub)[x]}', `
-                                }                                
+                                    if(key_man === "WHERE" || key_man === "AND" ||  key_man === "OR" ){
+                                            query += `${key_man} ${ Object.keys(key_sub)[0]} = '${ Object.values(key_sub)[0]}'  `
+                                    }
+                                }                     
                             }
                         }
                        
                     }
-                    let queryWhere1 = `${exe} ${query.slice(0, query.length -2)} ${order_by}`
-
-                    console.log(`${exe} ${query.slice(0, query.length -2)} ${order_by}`)
-
+                    let queryWhere1 = `${exe} ${query.slice(0, query.length -2)} ${ands} ${order_by}`
+                    //console.log(like2)
                     this.storage.transaction((ex)=>{
                         ex.executeSql(`SELECT * FROM ${_tableName} ${queryWhere1}`,[], results)
                     })
@@ -108,20 +138,141 @@ class WebSqlite {
                     
                 }
             }
-            
-            
         }
         /*
         *   Delete item 
         */
-        const deleteOne = ()=>{
-
+        const deleteOne = (args, callback)=>{
+            if( args === ""){
+                let res = "Error line 119, only accepte objects type {}"
+                if(typeof(callback) === "function"){
+                    callback(res)
+                }
+            }else{
+                if(typeof(args) === "object"){
+                    let query = ""
+                    let exe = ""
+                    let order_by = ""
+                    let ands= ""
+                    for(let i= 0; i < Object.keys(args).length; i++){
+                        
+                        if(Object.keys(args)[i] === "ORDER BY"){
+                            order_by = `${Object.keys(args)[i]} ${Object.values(args)[i]}`
+                        } 
+                        else if(Object.keys(args)[i] === "WHERE"){
+                            let key_man = Object.keys(args)[i]
+                            let key_sub = Object.values(args)[i]
+                            
+                            for(let x=0; x < Object.keys(key_sub).length; x++){
+                                exe = `${key_man}`
+                                
+                                if(typeof(Object.values(key_sub)[x]) === "object"){
+                                    let keys_sub2 = Object.values(key_sub)[x]
+                                    
+                                    if(Object.keys(key_sub)[x] === "AND" || Object.keys(key_sub)[x] === "OR"){
+                                        for(let y =0; y < Object.values(keys_sub2).length; y++){
+                                            ands += `${Object.keys(key_sub)[x]} ${Object.keys(keys_sub2)[y]} = '${Object.values(keys_sub2)[y]}'` 
+                                        }
+                                    }
+                                    else if(Object.keys(key_sub)[x] === "LIKE"){
+                                        for(let y =0; y < Object.values(keys_sub2).length; y++){
+                                            query += `${Object.keys(key_sub)[x]} ${Object.keys(keys_sub2)[y]} '${Object.values(keys_sub2)[y]}''`
+                                        }
+                                    }
+                                }
+                                else{
+                                    query += `${Object.keys(key_sub)[x]} = '${Object.values(key_sub)[x]}', `
+                                }                                
+                            }
+                        }
+                       
+                    }
+                    let queryWhere1 = `${exe} ${query.slice(0, query.length -2)} ${ands} ${order_by}`
+                    //console.log(queryWhere1)
+                    this.storage.transaction((ex)=>{
+                        ex.executeSql(`DELETE FROM ${_tableName} ${queryWhere1}`,[], results)
+                    })
+                    const results = (err, result)=>{
+                        
+                    }
+                    
+                }
+            }
         }
         /*
         *  Update from item 
         */
-        const update = ()=>{
-
+        const update = (args, callback)=>{
+            if( args === ""){
+                let res = "Error line 119, only accepte objects type {}"
+                if(typeof(callback) === "function"){
+                    callback(res)
+                }
+            }else{
+                if(typeof(args) === "object"){
+                    let query = ""
+                    let exe = ""
+                    let exe2 = ""
+                    let order_by = ""
+                    let ands= ""
+                    let set = ""
+                    for(let i= 0; i < Object.keys(args).length; i++){
+                        
+                        if(Object.keys(args)[i] === "ORDER BY"){
+                            order_by = `${Object.keys(args)[i]} ${Object.values(args)[i]}`
+                        } 
+                        else if(Object.keys(args)[i] === "SET"){
+                            let key_man = Object.keys(args)[i]
+                            let key_sub = Object.values(args)[i]
+                            
+                            for(let x=0; x < Object.keys(key_sub).length; x++){
+                                exe = `${key_man}`
+                                
+                                set += `${Object.keys(key_sub)[x]} = '${Object.values(key_sub)[x]}', `                                
+                            }
+                        }
+                        else if(Object.keys(args)[i] === "WHERE"){
+                            let key_man = Object.keys(args)[i]
+                            let key_sub = Object.values(args)[i]
+                            for(let x=0; x < Object.keys(key_sub).length; x++){
+                                exe2 = `${key_man}`
+                                
+                                if(typeof(Object.values(key_sub)[x]) === "object"){
+                                    let keys_sub2 = Object.values(key_sub)[x]
+                                    
+                                    if(Object.keys(key_sub)[x] === "AND" || Object.keys(key_sub)[x] === "OR"){
+                                        for(let y =0; y < Object.values(keys_sub2).length; y++){
+                                            ands += `${Object.keys(key_sub)[x]} ${Object.keys(keys_sub2)[y]} = '${Object.values(keys_sub2)[y]}'` 
+                                        }
+                                    }
+                                    else if(Object.keys(key_sub)[x] === "LIKE"){
+                                        for(let y =0; y < Object.values(keys_sub2).length; y++){
+                                            query += `${Object.keys(key_sub)[x]} ${Object.keys(keys_sub2)[y]} '${Object.values(keys_sub2)[y]}''`
+                                        }
+                                    }
+                                }
+                                else{
+                                    query += `${Object.keys(key_sub)[x]} = '${Object.values(key_sub)[x]}', `
+                                }                                
+                            }
+                        }
+                       
+                    }
+                    let queryWhere1 = `${exe} ${set.slice(0, set.length -2)} ${exe2} ${query.slice(0, query.length -2)} ${ands}`
+                    console.log(`UPDATE ${_tableName} ${queryWhere1}`)
+                    this.storage.transaction((ex)=>{
+                        ex.executeSql(`UPDATE ${_tableName} ${queryWhere1}`,[], results)
+                    })
+                    
+                    const results = (err, result)=>{
+                        let res = "Object updated..."
+                        if(typeof(callback) === "function"){
+                            callback(res)
+                        }
+                    }
+                    
+                }
+            }
         }
         /*
         *   DROP THIS TABLES
@@ -181,34 +332,11 @@ class WebSqlite {
     OP = ()=>{
         const LIKE = "LIKE"
         const ORDERBY= "ORDER BY"
-        return {LIKE, ORDERBY}
+        const OR = "OR"
+        const SET = "SET"
+        const AND = "AND"
+        const WHERE = "WHERE"
+        return {LIKE, ORDERBY,OR, AND, SET, WHERE}
     }
     
 }
-
-// user methods 
-const sqlite = new WebSqlite ("MyDb")
-const Types = sqlite.Types()
-const Op = sqlite.OP()
-
-const users = sqlite.Schema("users", {
-    name: Types.TEXT,
-    email: Types.TEXT,
-    number: Types.NUMBER,
-    password: Types.TEXT
-})
-//users.DROP()
-
-let data = { 
-    name: "admin",
-    email: "admin@gmail.com",
-    number: "945197584",
-    password: "1234"
-}
-
-users.findAll({
-    WHERE: {name: {[Op.LIKE]: "luca%"}}
-}, (datas)=>{ console.log(datas)})
-
-
-//users.create(data)
--- index.js
+++ index.js
@@ -1,75 +1,41 @@
-###  ================= Web-Squelizer Api (1.0) =================
-[
-    `Contacta-me`
-    `Email`: ([lucasveraz102000@gmail.com])
-    `Facebook`: [https://facebook.com/lveraz1]
-    `Telegram`: [https://t.me/muana_mayele]
-    `WhatsApp`: [https://wa.me/message/QVIXX2HAA2EQL1]
-    `GitHub`: [https://github.com/lukas102000?tab=repositories]
-    `Linkedin`: [https://www.linkedin.com/in/lucas-veraz-382185185]
-]
-
-
-`Documento oficial do api`
-[(https://google.com)]
-
-### `Description`
-### Web-Squelizer: 
-É uma API de conexão para `Banco de dados Web sql Locais` Nos navegadores.
-Ou seja, permite conectar e salvar dados local no Web Sql 
-Este Api permite `Criar e manipular os dados de forma fácil`, facilitando assim o tempo de codificação...
-
-`Web-Squelizer` é suportado apenas em navegadores recentes, como `Microsoft Edge Chrome, opera, opera min, Mozilla Firefox` no 
-`Windows, Maccos ou Linux`, e também em plataformas `mobiles` incluindo o `Phoenix e outros`
-
-### ========== USO =================
-
-## inicializando o api /Web-Squelizer
-````js
-
+// inicializando o api /Web-Squelizer
 const Websql = new WebSquelizer("Shops", (e)=>{ console.log(e)})
-`Types é retorna os tipos de dado usados para criação`
-
+// Types é retorna os tipos de dado usados
 const Types = Websql.Types()
-
-`Usando as opção do WebSquelizer para execução`
+// Usando as opção do WebSquelizer para execução
 const Op = Websql.OP()
 
-
-`
-*   Criando A primeira tabela de produtos!
+// Criando A primeira tabela de produtos
+/*
 *   Para criar a uma table ou executar comandos sqlite usa a função Schema()
 *   E adiciona o nome da tabela e o segundo parámetro querer um objectos ou seja colunas.
 *   Este método ele criar o id como primary key
-*
-`
+*/
 const produtos = Websql.Schema("produtos",{
     name: Types.TEXT,
     preco: Types.TEXT,
     date: Types.TEXT
 })
-`
+/*
 *   Agora inserir os dados, Para inserir os dados chamamos a função Create() com os dados
 *   Vamos usar a const produtos para podemos manipular a tabela produtos.
 *   Esta const produtos ele carrega todas as funções, por isso vamos usar ele
-`
+*/
 let data = {
     name: "Abacates",
     preco: "200,00kz",
     date: new Date()
 }
-` Criando ou inserindo dados...`
-    produtos.create(data)
+//produtos.create(data)
 
 
-
-`
+// 
+/*
 *   Selecionando os elementos no banco de dados
 *   Para selecionamos os dados usa a função findAll() É uma retorna os dados.
 *   Nesta função poderás executar diferentes comandos do mysql
-*
-`
-`********** executando sem nenhuma condição sql **********`
+*/
+// executando sem nenhuma condição sql
 produtos.findAll({[Op.ORDERBY]: "id DESC"},(datas)=>{ 
     // esta função de callback retorna dados em um objecto ou um array
     console.log(datas)
@@ -82,10 +48,9 @@ produtos.findAll({[Op.ORDERBY]: "id DESC"},(datas)=>{
         //console.log(datas[i].id)
     }
 })
-
-
-`**********   Usando where no find All *********`
-
+/*
+*   Usando where no find All
+*/
 produtos.findAll({WHERE:{id: 1}},(datas)=>{ 
     // esta função de callback retorna dados em um objecto ou um array
     //console.log(datas)
@@ -98,10 +63,9 @@ produtos.findAll({WHERE:{id: 1}},(datas)=>{
         //console.log(datas[i].id)
     }
 })
-
-
-`********   Usando where e like no find All `
-
+/*
+*   Usando where e like no find All 
+*/
 produtos.findAll({WHERE:{name:{[Op.LIKE]: "%AN%"}, [Op.ORDERBY]: {id: "DESC"} }},(datas)=>{ 
     // esta função de callback retorna dados em um objecto ou um array
     //console.log(datas)
@@ -114,8 +78,10 @@ produtos.findAll({WHERE:{name:{[Op.LIKE]: "%AN%"}, [Op.ORDERBY]: {id: "DESC"} }}
         //console.log(datas[i].id)
     }
 })
+/*
+*   Usando where e AND no find All 
+*/
 
-`************ Usando where e AND no find All *************`
 produtos.findAll({WHERE:{id: 1}, [Op.AND]: {preco: "500,00kz"}, [Op.OR]: {name: "Bananas"}},(datas)=>{ 
     // esta função de callback retorna dados em um objecto ou um array
     //console.log(datas)
@@ -129,20 +95,11 @@ produtos.findAll({WHERE:{id: 1}, [Op.AND]: {preco: "500,00kz"}, [Op.OR]: {name: 
     }
 })
 
-` ***************** Delete ************ `
-produtos.deleteOne({WHERE: {id: 1}}, (e)=>{ console.log(e)})
-
-`************** Update ****************`
+/*
+* Delete
+*/
+//produtos.deleteOne({WHERE: {id: 1}}, (e)=>{ console.log(e)})
+/*
+* Update
+*/
 produtos.update({[Op.SET]: {name: "Bananas", preco: "500,00kz"}, WHERE:{id: 2} }, ()=>{})
-
-````
-
-[
-    `Contacta-me`
-    `Email`: ([lucasveraz102000@gmail.com])
-    `Facebook`: [https://facebook.com/lveraz1]
-    `Telegram`: [https://t.me/muana_mayele]
-    `WhatsApp`: [https://wa.me/message/QVIXX2HAA2EQL1]
-    `GitHub`: [https://github.com/lukas102000?tab=repositories]
-    `Linkedin`: [https://www.linkedin.com/in/lucas-veraz-382185185]
-]
